68952b08a91e48e481837e050dff23a127fe2d79
Ruth Mesfun <ruth@flatironschool.com>
2017-09-21 22:54:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,19 @@
 # Your code goes here!
 class Anagram
+  anagram = Array.new
   attr_accessor :word
+
   def initialize(word)
     @word = word
   end
+
+  def match(word_list)
+    letter_word = @word.split('')
+    word_list.select {|word| 
+      word.split('').include?(letter_word)
+      << word
+      }
+  end
+  
 
 end
